f3f5f9d6b39f01502352669694640fa2346580c3
Ivan <ivan.radovic@acme.co.me>
2023-04-21 06:38:05 +0200

    Adding favorite place

---
+++ screens/AllPlaces/AllPlacesScreen.components.jsx
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+
+const AllPlacesScreen = () => {
+    return (
+        <View>
+            All places
+        </View>
+      );
+}
+ 
+export default AllPlacesScreen;
---
+++ screens/PlaceDetails/PlaceDetails.component.jsx
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+
+const PlaceDetailsScreen = () => {
+    return (
+        <View>
+            Place details
+        </View>
+
+      );
+}
+ 
+export default PlaceDetailsScreen;
